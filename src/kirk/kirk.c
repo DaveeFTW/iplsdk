7c19e197c11cea8282eed5b7953e3e747eabed59
Davee Morgan <git@davee.pro>
2019-08-29 19:28:20 +0100

    add kirk 4/7 support

--- src/kirk/kirk.c
+++ src/kirk/kirk.c
@@ -50,3 +50,61 @@ int kirk1(void *dst, const void *src)
     SYNC();
     return kirk->result;
 }
+
+int kirk4(void *dst, const void *src)
+{
+    clear_dcache();
+    PspKirkRegs *const kirk = KIRK_HW_REGISTER_ADDR;
+ 
+    kirk->command = 4; // encrypt operation
+    kirk->src_addr = MAKE_PHYS_ADDR(src);
+    kirk->dst_addr = MAKE_PHYS_ADDR(dst);
+ 
+    kirk->proc_phase = 1; // start processing
+ 
+    while((kirk->status & 0x11) == 0); // wait until processing complete
+ 
+    if (kirk->status & 0x10) // error occured
+    {
+        kirk->proc_phase = 2;
+ 
+        while((kirk->status & 2) == 0);
+ 
+        kirk->status_end = kirk->status;
+        SYNC();
+        return -1;
+    }
+ 
+    kirk->status_end = kirk->status;
+    SYNC();
+    return kirk->result;
+}
+
+int kirk7(void *dst, const void *src)
+{
+    clear_dcache();
+    PspKirkRegs *const kirk = KIRK_HW_REGISTER_ADDR;
+ 
+    kirk->command = 7; // decrypt operation
+    kirk->src_addr = MAKE_PHYS_ADDR(src);
+    kirk->dst_addr = MAKE_PHYS_ADDR(dst);
+ 
+    kirk->proc_phase = 1; // start processing
+ 
+    while((kirk->status & 0x11) == 0); // wait until processing complete
+ 
+    if (kirk->status & 0x10) // error occured
+    {
+        kirk->proc_phase = 2;
+ 
+        while((kirk->status & 2) == 0);
+ 
+        kirk->status_end = kirk->status;
+        SYNC();
+        return -1;
+    }
+ 
+    kirk->status_end = kirk->status;
+    SYNC();
+    return kirk->result;
+}
--- src/kirk/kirk.h
+++ src/kirk/kirk.h
@@ -4,7 +4,18 @@
 extern "C" {
 #endif //__cplusplus
 
+typedef struct
+{
+    unsigned int mode;
+    unsigned int unk_4;
+    unsigned int unk_8;
+    unsigned int keyslot;
+    unsigned int size;
+} Kirk47Header;
+
 int kirk1(void *dst, const void *src);
+int kirk4(void *dst, const void *src);
+int kirk7(void *dst, const void *src);
 
 #ifdef __cplusplus
 }

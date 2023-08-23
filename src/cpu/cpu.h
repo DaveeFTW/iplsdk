652a5661cc8cf671f4eddee12de9c0bfef52ae1e
Davee Morgan <git@davee.pro>
2023-08-23 22:58:11 +0000

    cpu: add bitrev, clz, and sync

--- src/cpu/cpu.h
+++ src/cpu/cpu.h
@@ -1,8 +1,29 @@
 #pragma once
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif //__cplusplus
+
+static inline uint32_t cpu_bitrev(uint32_t x)
+{
+    uint32_t res;
+    __asm("bitrev %0, %1" : "=r" (res) : "r"(x));
+    return res;
+}
+
+static inline uint32_t cpu_clz(uint32_t x)
+{
+    uint32_t res;
+    __asm("clz %0, %1" : "=r" (res) : "r"(x));
+    return res;
+}
+
+static inline void cpu_sync(void)
+{
+    __asm("sync");
+}
 
 void cpu_enable_interrupts(void);
 unsigned int cpu_get_status(void);

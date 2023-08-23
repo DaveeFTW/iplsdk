bef159885a6273016a3af1350c66316c77ab80d6
Davee Morgan <git@davee.pro>
2023-08-23 17:19:51 +0000

    add all interrupts

--- src/interrupt/interrupt.c
+++ src/interrupt/interrupt.c
@@ -1,12 +1,105 @@
 #include "interrupt.h"
 
+#include <stdint.h>
+
+#define REG32(addr)                         ((volatile uintptr_t *)(addr))
+#define INTERRUPT_MMIO_BASE                 (0xBC300000)
+
+#define INTERRUPT_CTRL0_UNMASKED_FLAGS_REG  (REG32(INTERRUPT_MMIO_BASE + 0x00))
+#define INTERRUPT_CTRL0_FLAGS_REG           (REG32(INTERRUPT_MMIO_BASE + 0x04))
+#define INTERRUPT_CTRL0_MASK_REG            (REG32(INTERRUPT_MMIO_BASE + 0x08))
+
+#define INTERRUPT_CTRL1_UNMASKED_FLAGS_REG  (REG32(INTERRUPT_MMIO_BASE + 0x10))
+#define INTERRUPT_CTRL1_FLAGS_REG           (REG32(INTERRUPT_MMIO_BASE + 0x14))
+#define INTERRUPT_CTRL1_MASK_REG            (REG32(INTERRUPT_MMIO_BASE + 0x18))
+
+#define INTERRUPT_CTRL2_UNMASKED_FLAGS_REG  (REG32(INTERRUPT_MMIO_BASE + 0x20))
+#define INTERRUPT_CTRL2_FLAGS_REG           (REG32(INTERRUPT_MMIO_BASE + 0x24))
+#define INTERRUPT_CTRL2_MASK_REG            (REG32(INTERRUPT_MMIO_BASE + 0x28))
+
+#define INTR_MASK(x)                        (1 << (x % 32))
+
+#define ALWAYS_ENABLED_INTERRUPTS           (INTR_MASK(IRQ_ALL_UART) | INTR_MASK(IRQ_ALL_SPI) | INTR_MASK(IRQ_ALL_TIMERS) | INTR_MASK(IRQ_ALL_USB) | INTR_MASK(IRQ_UNK29))
+
+static inline void sync(void)
+{
+    __asm("sync");
+}
+
+static inline uint32_t *enabled_interrupts(void)
+{
+    static uint32_t masks[2] = { 0 };
+    return &masks[0];
+}
+
+static inline int is_ctrl2_interrupt(enum InterruptType interrupt)
+{
+    return interrupt == IRQ_UNK38 || interrupt == IRQ_UNK39;
+}
+
+static inline int is_ctrl1_interrupt(enum InterruptType interrupt)
+{
+    return interrupt >= 32 && !is_ctrl2_interrupt(interrupt);
+}
+
+static inline int is_ctrl0_interrupt(enum InterruptType interrupt)
+{
+    return interrupt < 32;
+}
+
+static inline void set_interrupt_masks(uint32_t *interrupts)
+{
+    *INTERRUPT_CTRL0_MASK_REG = interrupts[0] | ALWAYS_ENABLED_INTERRUPTS;
+    *INTERRUPT_CTRL1_MASK_REG = interrupts[1] & ~(INTR_MASK(IRQ_UNK38) | INTR_MASK(IRQ_UNK39));
+    *INTERRUPT_CTRL2_MASK_REG = (interrupts[2] & (INTR_MASK(IRQ_UNK38) | INTR_MASK(IRQ_UNK39))) >> 6;
+    sync();
+}
+
+void interrupt_init(void)
+{
+    uint32_t mask = interrupt_suspend();
+    uint32_t *interrupts = enabled_interrupts();
+    interrupts[0] = interrupts[1] = 0;
+    set_interrupt_masks(interrupts);
+    interrupt_resume(mask);
+}
+
+void interrupt_enable(enum InterruptType interrupt)
+{
+    uint32_t mask = interrupt_suspend();
+
+    uint32_t *interrupts = enabled_interrupts();
+    interrupts[interrupt/32] = (1 << (interrupt % 32));
+    set_interrupt_masks(interrupts);
+    interrupt_resume(mask);
+}
+
 int interrupt_occured(enum InterruptType interrupt)
 {
-    unsigned int occured = *(volatile unsigned int *)0xBC300000;
-    return occured & (1 << interrupt);
+    if (is_ctrl0_interrupt(interrupt)) {
+        return *INTERRUPT_CTRL0_UNMASKED_FLAGS_REG & INTR_MASK(interrupt);
+    }
+
+    else if (is_ctrl1_interrupt(interrupt)) {
+        return *INTERRUPT_CTRL1_UNMASKED_FLAGS_REG & INTR_MASK(interrupt);
+    }
+    
+    else { // if (is_ctrl2_interrupt(interrupt)) {
+        return *INTERRUPT_CTRL2_UNMASKED_FLAGS_REG & (INTR_MASK(interrupt) >> 6);
+    }
 }
 
 void interrupt_clear(enum InterruptType interrupt)
 {
-    *(volatile unsigned int *)0xBC300000 = (1 << interrupt);
+    if (is_ctrl0_interrupt(interrupt)) {
+        *INTERRUPT_CTRL0_UNMASKED_FLAGS_REG = INTR_MASK(interrupt);
+    }
+
+    else if (is_ctrl1_interrupt(interrupt)) {
+        *INTERRUPT_CTRL1_UNMASKED_FLAGS_REG = INTR_MASK(interrupt);
+    }
+    
+    else { // if (is_ctrl2_interrupt(interrupt)) {
+        *INTERRUPT_CTRL2_UNMASKED_FLAGS_REG = (INTR_MASK(interrupt) >> 6);
+    }
 }
--- src/interrupt/interrupt.h
+++ src/interrupt/interrupt.h
@@ -6,18 +6,81 @@ extern "C" {
 
 enum InterruptType
 {
-    IRQ_KIRK = 24
+    IRQ_ALL_UART = 0,
+    IRQ_ALL_SPI,
+    IRQ_ALL_TIMERS,
+    IRQ_ALL_USB,
+    IRQ_GPIO,
+    IRQ_ATA,
+    IRQ_UMDMAN,
+    IRQ_MS,
+    IRQ_WLAN,
+    IRQ_MG,
+    IRQ_AUDIO1,
+    IRQ_AUDIO2,
+    IRQ_I2C,
+    IRQ_KEY,
+    IRQ_IRDA,
+    IRQ_SYSTIMER0,
+    IRQ_SYSTIMER1,
+    IRQ_SYSTIMER2,
+    IRQ_SYSTIMER3,
+    IRQ_THREAD0,
+    IRQ_NAND,
+    IRQ_DMACPLUS,
+    IRQ_DMA0,
+    IRQ_DMA1,
+    IRQ_KIRK,
+    IRQ_GE,
+    IRQ_USB,
+    IRQ_EFLASH_ATA,
+    IRQ_EFLASH_DMA,
+    IRQ_UNK29,
+    IRQ_VSYNC,
+    IRQ_MEDIAENGINE,
+    IRQ_UART1,
+    IRQ_UART2,
+    IRQ_UART3,
+    IRQ_UART4,
+    IRQ_UART5,
+    IRQ_UART6,
+    IRQ_UNK38,
+    IRQ_UNK39,
+    IRQ_SPI1,
+    IRQ_SPI2,
+    IRQ_SPI3,
+    IRQ_SPI4,
+    IRQ_SPI5,
+    IRQ_SPI6,
+    IRQ_UNK46,
+    IRQ_UNK47,
+    IRQ_APB_TIMER0,
+    IRQ_APB_TIMER1,
+    IRQ_APB_TIMER2,
+    IRQ_APB_TIMER3,
+    IRQ_UNK52,
+    IRQ_UNK53,
+    IRQ_UNK54,
+    IRQ_UNK55,
+    IRQ_USB_RESUME,
+    IRQ_USB_READY,
+    IRQ_USB_ATTACH,
+    IRQ_USB_DETACH,
+    IRQ_MS_INSERT0,
+    IRQ_MS_INSERT1,
+    IRQ_UNK_WLAN0,
+    IRQ_UNK_WLAN1,
 };
 
 void interrupt_init(void);
 void interrupt_enable(enum InterruptType interrupt);
 
+int interrupt_occured(enum InterruptType interrupt);
+void interrupt_clear(enum InterruptType interrupt);
+
 unsigned int interrupt_suspend(void);
 void interrupt_resume(unsigned int mask);
 void interrupt_resume_with_sync(unsigned int mask);
-
-int interrupt_occured(enum InterruptType interrupt);
-void interrupt_clear(enum InterruptType interrupt);
 
 #ifdef __cplusplus
 }

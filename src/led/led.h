217dd6f6cbfc09acfa4639bb7cabb779f78a7733
Davee Morgan <git@davee.pro>
2023-11-03 02:36:37 +0000

    led: add led states

--- src/led/led.h
+++ src/led/led.h
@@ -13,7 +13,21 @@ enum Led {
     LED_BLUETOOTH
 };
 
+enum LedMode {
+    LED_MODE_ON,
+    LED_MODE_OFF,
+    LED_MODE_BLINK
+};
+
+typedef struct
+{
+    unsigned int on_time;
+    unsigned int off_time;
+    unsigned int blink_time;
+} LedConfig;
+
 void led_init(void);
+void led_set_mode(enum Led led, enum LedMode mode, const LedConfig *config);
 
 #ifdef __cplusplus
 }
